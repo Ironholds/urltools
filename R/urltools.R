d86f82f24df97da04b4ab65db38a283e05e55426
ironholds <ironholds@gmail.com>
2014-12-21 09:38:57 -0500

    docs

--- R/urltools.R
+++ R/urltools.R
@@ -4,6 +4,8 @@
 #' parsing, and parameter extraction, designed to be both fast and
 #' entirely vectorised. It is intended to be useful for people dealing with
 #' web-related datasets, such as server-side logs.
+#' 
+#' @seealso \link{url_decode}, \link{url_encode}, \link{url_extract_param}, \link{url_replace_param}, \link{parse_url}
 #' @useDynLib urltools
 #' @importFrom Rcpp sourceCpp
 #' @docType package
