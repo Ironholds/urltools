adfef33972f8bf6a649e88ae83f4f0928f26097f
ironholds <ironholds@gmail.com>
2014-12-21 09:43:14 -0500

    restructure

--- src/encoding.h
+++ src/encoding.h
@@ -1,6 +1,9 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#ifndef __ENCODING_INCLUDED__
+#define __ENCODING_INCLUDED__
+
 //Encoding handlers
 class encoding{
   
@@ -45,52 +48,10 @@ class encoding{
 
   public:
 
-    static std::string internal_url_decode(std::string URL){
-      
-      //Create output object
-      std::string result;
-      
-      //For each character...
-      for (std::string::size_type i = 0; i <  URL.size(); ++i){
-        
-        //If it's a +, space
-        if (URL[i] == '+'){
-          result += ' ';
-        } else if (URL[i] == '%' && URL.size() > i+2){//Escaped? Convert from hex and includes
-          char holding_1 = from_hex(URL[i+1]);
-          char holding_2 = from_hex(URL[i+2]);
-          char holding = (holding_1 << 4) | holding_2;
-          result += holding;
-          i += 2;
-        } else { //Permitted? Include.
-          result += URL[i];
-        }
-      }
-      
-      //Return
-        return result;
-    }
+    static std::string internal_url_decode(std::string URL);
     
-    static std::string urlencode(std::string & url){
+    static std::string urlencode(std::string & url);
 
-      //Note the unreserved characters, create an output string
-      std::string unreserved_characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._~-";
-      std::string output = "";
-      
-      //For each character..
-      for(int i=0; i < (signed) url.length(); i++){
-        
-        //If it's in the list of reserved ones, just pass it through
-        if (unreserved_characters.find_first_of(url[i]) != std::string::npos){
-          output.append(&url[i], 1);
-        //Otherwise, append in an encoded form.
-        } else {
-          output.append("%");
-          output.append(to_hex(url[i]));
-        }
-      }
-      
-      //Return
-      return output;
-    }
 };
+
+#endif
