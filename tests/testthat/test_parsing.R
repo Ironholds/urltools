cc9838e9d9eba4f262ec864f2a5fac4becbb8ef1
ironholds <ironholds@gmail.com>
2015-04-27 12:38:33 -0400

    expand and rewrite unit tests

--- tests/testthat/test_parsing.R
+++ tests/testthat/test_parsing.R
@@ -2,24 +2,28 @@ context("URL parsing tests")
 
 test_that("Check parsing identifies each RfC element", {
   
-  data <- unlist(url_parse("https://www.google.com:80/foo.php?api_params=turnip#ending"))
-  expect_that(data[1], equals("https"))
-  expect_that(data[2], equals("www.google.com"))
-  expect_that(data[3], equals("80"))
-  expect_that(data[4], equals("foo.php"))
-  expect_that(data[5], equals("api_params=turnip"))
-  expect_that(data[6], equals("ending"))
+  data <- url_parse("https://www.google.com:80/foo.php?api_params=turnip#ending")
+  expect_that(ncol(data), equals(6))
+  expect_that(names(data), equals(c("scheme","domain","port","path","parameter","fragment")))
+  expect_that(data$scheme[1], equals("https"))
+  expect_that(data$domain[1], equals("www.google.com"))
+  expect_that(data$port[1], equals("80"))
+  expect_that(data$path[1], equals("foo.php"))
+  expect_that(data$parameter[1], equals("api_params=turnip"))
+  expect_that(data$fragment[1], equals("ending"))
 })
 
 test_that("Check parsing can handle missing elements", {
   
-  data <- unlist(url_parse("https://www.google.com/foo.php?api_params=turnip#ending"))
-  expect_that(data[1], equals("https"))
-  expect_that(data[2], equals("www.google.com"))
-  expect_that(data[3], equals(""))
-  expect_that(data[4], equals("foo.php"))
-  expect_that(data[5], equals("api_params=turnip"))
-  expect_that(data[6], equals("ending"))
+  data <- url_parse("https://www.google.com/foo.php?api_params=turnip#ending")
+  expect_that(ncol(data), equals(6))
+  expect_that(names(data), equals(c("scheme","domain","port","path","parameter","fragment")))
+  expect_that(data$scheme[1], equals("https"))
+  expect_that(data$domain[1], equals("www.google.com"))
+  expect_that(data$port[1], equals(""))
+  expect_that(data$path[1], equals("foo.php"))
+  expect_that(data$parameter[1], equals("api_params=turnip"))
+  expect_that(data$fragment[1], equals("ending"))
 })
 
 test_that("Parameter parsing can handle multiple, non-existent and pre-trailing parameters",{
