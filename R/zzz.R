231831a30ad9edad38dd3e428576887dda016b11
Maryam Khezrzadeh <maryam.khezrzadeh@gmail.com>
2016-08-25 14:50:43 -0700

    Fixes issue #48: suffix_extract extracts the domain wrong

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ suffix_load <- function(suffixes = NULL){
   if(is.null(suffixes)){
     suffixes <- urltools::suffix_dataset
   }
-  cleaned_suffixes <- gsub(x = urltools::suffix_dataset, pattern = "*.", replacement = "", fixed = TRUE)
+  cleaned_suffixes <- gsub(x = urltools::suffix_dataset, pattern = ".", replacement = "", fixed = TRUE)
   is_wildcard <- cleaned_suffixes[which(grepl(x = urltools::suffix_dataset, pattern = "*.", fixed = TRUE))]
   assign("is_wildcard", is_wildcard, envir = urltools_env)
   suff_trie <- triebeard::trie(keys = reverse_strings(cleaned_suffixes),
