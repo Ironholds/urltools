a8c0b4ed83443261922aceb910e16b222efd6743
Kirill Pomogajko <kirill@adblockplus.org>
2019-05-06 15:15:55 +0200

    get the suffixes array if suffixes = NULL

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@ urltools_env <- new.env(parent = emptyenv())
 suffix_load <- function(suffixes = NULL){
   if(is.null(suffixes)){
     suffixes <- urltools::suffix_dataset
+    suffixes <- suffixes$suffixes
   }
   cleaned_suffixes <- gsub(x = suffixes, pattern = "*.", replacement = "", fixed = TRUE)
   is_wildcard <- cleaned_suffixes[which(grepl(x = suffixes, pattern = "*.", fixed = TRUE))]
